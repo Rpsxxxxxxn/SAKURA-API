df8f2197b37ca7fa1d8c7082fb4bb64feef09634
Rpsxxxxxxn <keito0122.y@gmail.com>
2022-11-30 17:07:17 +0900

    コントローラーの更新

--- src/controllers/ExperienceController.ts
+++ src/controllers/ExperienceController.ts
@@ -3,18 +3,23 @@ import { ExperienceDto } from './dto/ExperienceDto';
 
 @JsonController()
 export class ExperienceController {
-    @Get('/experiences')
-    getAll(@QueryParams() query: any) {
-        console.log(query);
-        return `All`;
-    }
 
+    /**
+     * 経験値の設定
+     * @param body 
+     * @returns 
+     */
     @Post('/experiences')
     post(@Body() body: ExperienceDto) {
         console.log(body);
         return `post`;
     }
 
+    /**
+     * 経験値の削除
+     * @param id 
+     * @returns 
+     */
     @Delete('/experiences/:id')
     delete(@Param('id') id: number) {
         console.log(id);
--- src/controllers/ServerListController.ts
+++ src/controllers/ServerListController.ts
@@ -1,21 +1,35 @@
 import { Body, Delete, Get, JsonController, Param, Post, QueryParams, UseAfter } from "routing-controllers";
-import { LoggingMiddleware } from "../shared/middleware/LoggingMiddleware";
 import { ServerListDto } from './dto/ServerListDto';
 
 @JsonController()
 export class ServerListController {
+    /**
+     * サーバリスト全取得
+     * @param query 
+     * @returns 
+     */
     @Get('/serverlist')
-    getAll(@QueryParams() query: any) {
+    findAll(@QueryParams() query: any) {
         console.log(query);
         return `All`;
     }
 
+    /**
+     * サーバ情報の設定
+     * @param body ServerListDto
+     * @returns 
+     */
     @Post('/serverlist')
     post(@Body() body: ServerListDto) {
         console.log(body);
         return `post`;
     }
 
+    /**
+     * サーバ情報の削除
+     * @param id 
+     * @returns 
+     */
     @Delete('/serverlist/:id')
     delete(@Param('id') id: number) {
         console.log(id);
